dc4df46c0367d4d319097218bfb7c805219b0fe7
monashr <mohammednasserkdw@gmail.com>
2024-06-24 11:07:35 +0700

    v3

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	base: '/TBA2024',
+	//base: '/TBA2024',
 	plugins: [sveltekit()]
 });
